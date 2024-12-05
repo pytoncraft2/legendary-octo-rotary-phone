4ef8050f716bb80530ac50d803fc76ae3ffb4551
pythoncraft2 <pythoncraft2@gmail.com>
2024-12-05 14:12:29 +0100

    feat(home): simple test of $state variable
    Add a with value 43 and 'On est {a}'
    
    plus d'info : https://svelte.dev/

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    let a = $state(43)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<h1>On est {a}</h1>
